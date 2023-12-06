276b09fee0f2b3b6e685b549e7d8c1841f9a7b63
BauerPhilipp <fam.bauer@live.at>
2023-12-06 18:12:04 +0100

    Camera Zoom

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -9,10 +9,17 @@ public class Unit : MonoBehaviour
 
 
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
     }
 
     private void Update()
@@ -34,9 +41,12 @@ public class Unit : MonoBehaviour
             unitAnimator.SetBool("IsWalking", false);
         }
 
-
-
-
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if(newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition; 
+        }
     }
 
     public void Move(Vector3 targetPosition)
